832e804d753c3add4e730a60afbce2ab13836e29
fine <fine_951111@163.com>
2025-12-09 17:00:03 +0800

    feat: add permissions module for authentication.

--- external/jx3auth/src/permissions/index.ts
+++ external/jx3auth/src/permissions/index.ts
@@ -10,11 +10,47 @@ declare module "koishi" {
     userServer: string; //角色区服
   }
 }
+
+const whiteList = [
+  "绑定区服",
+  "绑定角色",
+  "续期",
+  "添加时间",
+  "状态",
+  "开启开服监控",
+  "关闭开服监控",
+  "开启新闻播报",
+  "关闭新闻播报",
+  "开启更新播报",
+  "关闭更新播报",
+  "开启奇遇播报",
+  "关闭奇遇播报",
+  "开启八卦播报",
+  "关闭八卦播报",
+  "开启马驹播报",
+  "关闭马驹播报",
+  "开启扶摇播报",
+  "关闭扶摇播报",
+  "开启烟花播报",
+  "关闭烟花播报",
+  "开启玄晶播报",
+  "关闭玄晶播报",
+  "开启的卢播报",
+  "关闭的卢播报",
+  "开启据点播报",
+  "关闭据点播报",
+  "开启帮会宣战",
+  "关闭帮会宣战",
+  "开启追魂播报",
+  "关闭追魂播报",
+  "开启诛恶事件",
+  "关闭诛恶事件",
+];
+
 export function setupPermissions(ctx: Context, config: Config) {
   //校验权限
   ctx.on("command/before-execute", async ({ session, ...argv }) => {
     //白名单指令数组
-    const whiteList = ["绑定区服", "绑定角色", "续期", "添加时间", "状态"];
     if (whiteList.includes(argv.command.name)) return;
 
     //验证服务是否过期
